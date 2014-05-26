e1ecaef13b1d6f9e33dc543e78fcc47ab407953f
tory <tadams@thegameclash.com>
2014-05-25 22:06:29 -0700

    base app

--- routes/index.js
+++ routes/index.js
@@ -10,63 +10,37 @@ var twitter = new twitterAPI({
     callback: 'http://localhost:3000/tweets'
 });
 
-var CronJob = require("cron").CronJob;
+var db = require('orchestrate')("104758bd-7e1e-4023-9000-12437f96a5a4");
 
+var Twit = require('twit')
+
+var T = new Twit({
+    consumer_key:         '...'
+  , consumer_secret:      '...'
+  , access_token:         '...'
+  , access_token_secret:  '...'
+});
+
+var stream = T.stream('statuses/filter', {
+  track: 'gaming, games, #gameclash, #gamedev, xbox, xboxlive, xbox360, xboxone, #xboxlive, #cod',
+  language: 'en'
+});
+/** the stream **/
+stream.on('tweet', function (tweet) {
+  db.put("tweets", tweet.id, tweet);
+});
 
 /** orchestrate info **/
 
-var db = require('orchestrate')("104758bd-7e1e-4023-9000-12437f96a5a4");
 
-
-db.get("twitter_creds", "usercred")
-  .then(function (results){
-    if(results.body.access && results.body.secret){
-      twitter.search("show", {q: "gaming"}, results.body.access, results.body.secret,
-        function (error, data, response){
-          console.log("error :      " + error);
-          console.log("data :      " + data);
-          console.log("response :      " + response);
-        });
-    }
-  });
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  // db.get("twitter_creds", "usercred")
-  //   .then(function (results){
-  //
-  //   });
-  twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
-    if (error) {
-        console.log("Error getting OAuth request token : " + error);
-    } else {
-      db.put("twitter_creds", "tweetbot", {
-        key: requestToken,
-        secret: requestTokenSecret
-      });
-      res.redirect("https://twitter.com/oauth/authenticate?oauth_token=" + requestToken);
-    }
-  });
+
 });
 
-router.get('/tweets', function (req, res){
-  var oauth_verifier = req.param("oauth_verifier");
-  var requestToken = req.param("oauth_token");
-  db.get("twitter_creds", "tweetbot")
-    .then(function (results){
+router.get('/gameclash_tweets', function (req, res){
 
-      secretToken = results.body.secret;
-      twitter.getAccessToken(requestToken, secretToken, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
-        if (error) {
-            console.log(error);
-        } else {
-            db.put("twitter_creds", "usercred", {
-              access: accessToken,
-              secret: accessTokenSecret
-            });
-        }
-      });
-    });
 });
 
 module.exports = router;

2db506b9a0755aeccb9a44581a88966ccc645028
tory <tadams@thegameclash.com>
2014-05-27 17:52:30 -0700

    botty

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
-/** twitter api module **/
-var twitterAPI = require('node-twitter-api');
-var twitter = new twitterAPI({
-    consumerKey: 'nwTesjrCxeKdLtCGATFMJSQXf',
-    consumerSecret: 'N4LadDlA4d0yuLuvrMWKEk1S6H1woqhdXY31dSHkJH7VEbvDOx',
-    callback: 'http://localhost:3000/tweets'
-});
-
-var db = require('orchestrate')("104758bd-7e1e-4023-9000-12437f96a5a4");
-
-var Twit = require('twit')
+var db = require('orchestrate')("104758bd-7e1e-4023-9000-12437f96a5a4")
+, Twit = require('twit')
 
 var T = new Twit({
     consumer_key:         'nwTesjrCxeKdLtCGATFMJSQXf'
@@ -20,7 +10,7 @@ var T = new Twit({
   , access_token:         '212382364-wjqwsJzZvqOzdoGVYbRQzv5JxPcmHfiUtJuYYXgc'
   , access_token_secret:  'QuV99OzEBDijkMB8FIe3Z17xVyCYnHoxJz5EJrg7QZYe7'
 });
-
+//
 var stream = T.stream('statuses/filter', {
   track: 'gaming, games, #gameclash, #gamedev, xbox, xboxlive, xbox360, xboxone, #xboxlive, #cod',
   language: 'en'
@@ -36,11 +26,27 @@ stream.on('tweet', function (tweet) {
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send("works"); 
+  db.search("tweets", "*")
+    .then(function (results){
+
+    })
+    .fail(function (err){
+
+    });
+    res.render("index");
 });
 
 router.get('/gameclash_tweets', function (req, res){
+  if(!req.param("page")){
+    res.redirect("/");
+  } else {
 
+    var offset = parseInt(req.param("page") * 100);
+    // db.search("tweets", "#gameclash^100")
+    //   .then(function (results){
+    //
+    //   });
+  }
 });
 
 module.exports = router;

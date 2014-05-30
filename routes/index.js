6abe7fedc7ccda030a6a6865965f4bb2d55f3a86
tory <tadams@thegameclash.com>
2014-05-30 16:10:10 -0500

    boilerplate

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var db = require('orchestrate')("104758bd-7e1e-4023-9000-12437f96a5a4")
+var db = require('orchestrate')("...")
 , Twit = require('twit')
 
 var T = new Twit({
-    consumer_key:         'nwTesjrCxeKdLtCGATFMJSQXf'
-  , consumer_secret:      'N4LadDlA4d0yuLuvrMWKEk1S6H1woqhdXY31dSHkJH7VEbvDOx'
-  , access_token:         '212382364-wjqwsJzZvqOzdoGVYbRQzv5JxPcmHfiUtJuYYXgc'
-  , access_token_secret:  'QuV99OzEBDijkMB8FIe3Z17xVyCYnHoxJz5EJrg7QZYe7'
+    consumer_key:         '...'
+  , consumer_secret:      '...'
+  , access_token:         '...'
+  , access_token_secret:  '...'
 });
 //
 var stream = T.stream('statuses/filter', {

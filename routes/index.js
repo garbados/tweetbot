d467af8b55aa35b8ee0ba92569e6e6fb64ea670f
tory <tadams@thegameclash.com>
2014-06-03 15:33:30 -0700

    personal

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ var db = require('orchestrate')("104758bd-7e1e-4023-9000-12437f96a5a4")
 , Twit = require('twit')
 
 var T = new Twit({
-    consumer_key:         'nwTesjrCxeKdLtCGATFMJSQXf'
-  , consumer_secret:      'N4LadDlA4d0yuLuvrMWKEk1S6H1woqhdXY31dSHkJH7VEbvDOx'
-  , access_token:         '212382364-wjqwsJzZvqOzdoGVYbRQzv5JxPcmHfiUtJuYYXgc'
-  , access_token_secret:  'QuV99OzEBDijkMB8FIe3Z17xVyCYnHoxJz5EJrg7QZYe7'
+    consumer_key:         '9yarKPLzBoVsi27Aw6rCVbDih'
+  , consumer_secret:      'yJQ4BoBtRHkK75VbRrvSRFyHaTg4G47aQrg0nLaa89ICZpVl5f'
+  , access_token:         '2535857940-tXxISLLvCF0vmG1F1fDnccbhUprUEQkHBWyW3rV'
+  , access_token_secret:  '82C54iOMa6AtjNex3jG5sRdzct1I4T7HiolUVOziWN3Bm'
 });
+
+
 //
 var stream = T.stream('statuses/filter', {
   track: '#gaming, #videogames, #games, #gameclash, #gamedev, #xbox, #xboxlive, #xbox360, #xboxone, #xboxlive, #cod',
@@ -17,6 +19,8 @@ var stream = T.stream('statuses/filter', {
 });
 /** the stream **/
 stream.on('tweet', function (tweet) {
+  T.post('statuses/retweet/:id', { id: tweet.id }, function (err, data, response) {
+})
   db.put("tweets", tweet.id, tweet);
 });
 
